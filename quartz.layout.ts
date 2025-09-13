4c19dba669b69e110333056cea1ce1f617199a99
Gassandrid <pedersenewan@gmail.com>
2025-09-12 21:05:22 -0400

    giscus

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: "giscus",
+      options: {
+        // from data-repo
+        repo: "Gassandrid/crdg.xyz",
+        // from data-repo-id
+        repoId: "R_kgDOPRFPcA",
+        // from data-category
+        category: "Announcements",
+        // from data-category-id
+        categoryId: "DIC_kwDOPRFPcM4CvYpY",
+      },
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/Gassandrid/crdg.xyz",
